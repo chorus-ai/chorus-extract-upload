590f38fc964c840625e2aa1bd5cb93cc9cdb5a6d
wa6gz <wa6gz@virginia.edu>
2024-05-24 15:52:53 -0400

    Restructure file manifest-journal and remove registry component. Updating to use filename-size-modtime matching, rather than computing MD5 for every element.

--- upload_manifest/generate_manifest/generate_manifest.py
+++ upload_manifest/generate_manifest/generate_manifest.py
@@ -4,6 +4,18 @@ import os
 import glob
 import hashlib
 from datetime import datetime
+import pandas as pd
+
+
+# TODO: DONE - Remove registry
+# TODO: DONE - Change schema of files to include modtime, filesize
+# TODO: Change matching to look at all three (name, time, size) 
+# TODO: calculate MD5 for all non-matching files
+# TODO: DONE Set "ready-to-sync" field for all non-matches
+# TODO: Update to use paths instead of filenames
+# TODO: Datetime format
+
+# QUESTION: Datetimes local or unix?
 
 # current assumptions/working decisions for initial run:
 # 1 file per record - can be extended to 1 dir per record or using a file-record mapping
@@ -20,22 +32,16 @@ def gen_manifest(folderpath, databasename="journal.db"):
 
     cur = con.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS registry(\
-                RECORD_ID INTEGER NOT NULL PRIMARY KEY, \
-                PERSON_ID INTEGER, \
-                SESSION_ID INTEGER, \
-                DATE TEXT, \
-                TIME TEXT, \
-                UPLOADDIR TEXT, \
-                ISACTIVE INTEGER)")
-    cur.execute("CREATE TABLE IF NOT EXISTS source_files(\
+    cur.execute("CREATE TABLE IF NOT EXISTS manifest(\
                 FILE_ID INTEGER NOT NULL PRIMARY KEY, \
-                RECORD_ID INTEGER, \
                 PERSON_ID INTEGER, \
-                SRCFILE TEXT, \
+                FILENAME TEXT, \
+                MODTIME TEXT, \
+                SIZE INTEGER, \
                 MD5 TEXT, \
                 TIME_VALID TEXT, \
-                TIME_INVALID TEXT)")
+                TIME_INVALID TEXT, \
+                SYNC_READY INTEGER)")
         
     globpath = os.path.join(folderpath, "*", "Waveforms","*")
     # print(globpath)
@@ -50,15 +56,17 @@ def gen_manifest(folderpath, databasename="journal.db"):
         pathstem = os.path.split(path)[0]
         filename = os.path.split(path)[1]
         personid = os.path.split(os.path.split(pathstem)[0])[1]
-        mydate = filename.split("_")[1]
-        mytime = filename.split("_")[2]
+        statinfo = os.stat(path)
+        filesize = statinfo.st_size
+        modtimestamp = statinfo.st_mtime
+        moddatetime = pd.to_datetime(modtimestamp, unit='s')
+        moddt_string = moddatetime.strftime("%Y%m%d_%H%M%S")
         mymd5 = hashlib.md5(open(path, 'rb').read()).hexdigest()
-        myargs = (i_path, personid,i_path, mydate,mytime, "",1)
-        print(myargs)
+        # myargs = (i_path, personid,i_path, mydate,mytime, "",1)
+        # print(myargs)
         # print([type(item) for item in myargs])        
-        cur.executemany("INSERT INTO registry VALUES(?, ?, ?, ?, ?, ?, ?)", [myargs])
-        myargs = (i_path,i_path, personid,filename, mymd5,dt_string, None)
-        cur.executemany("INSERT INTO source_files VALUES(?, ?, ?, ?, ?, ?, ?)", [myargs])
+        myargs = (i_path, personid,filename,moddt_string,filesize, mymd5,dt_string, None,1)
+        cur.executemany("INSERT INTO manifest VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", [myargs])
 
     con.commit() 
     con.close()
@@ -81,34 +89,35 @@ def update_manifest(folderpath, databasename="journal.db"):
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d_%H:%M:%S")
     dt_nosymbols = now.strftime("%Y%m%d_%H%M%S")
-    registrybackup = "registry_" + dt_nosymbols
-    cur.execute(f"DROP TABLE IF EXISTS {registrybackup}")
-    cur.execute(f"CREATE TABLE {registrybackup} AS SELECT * FROM registry")
 
-    filesbackup = "source_files_" + dt_nosymbols
+    filesbackup = "manifest_" + dt_nosymbols
     cur.execute(f"DROP TABLE IF EXISTS {filesbackup}")
-    cur.execute(f"CREATE TABLE {filesbackup} AS SELECT * FROM source_files")
+    cur.execute(f"CREATE TABLE {filesbackup} AS SELECT * FROM manifest")
 
-    i_file = cur.execute("SELECT MAX(file_id) from source_files").fetchone()[0] + 1
-    print(i_file)
-    i_record = cur.execute("SELECT MAX(record_id) from registry").fetchone()[0] + 1
-    print(i_record)
+    i_file = cur.execute("SELECT MAX(file_id) from manifest").fetchone()[0] + 1
+    # print(i_file)
 
     # get all active files, these are active-pending-delete until the file is found in filesystem
-    activefiletuple = cur.execute("Select file_id, record_id from source_files where TIME_INVALID IS NULL").fetchall()
+    activefiletuple = cur.execute("Select file_id from manifest where TIME_INVALID IS NULL").fetchall()
     
-    activefilemap = {i[0]:i[1] for i in activefiletuple}
+    activefileset = {i[0] for i in activefiletuple}
+
+    cur.execute("UPDATE manifest SET SYNC_READY=?", (0,))
+
     for i_path, path in enumerate(paths):
-        print(path)
+        # print(path)
         pathstem = os.path.split(path)[0]
         filename = os.path.split(path)[1]
         personid = os.path.split(os.path.split(pathstem)[0])[1]
-        mydate = filename.split("_")[1]
-        mytime = filename.split("_")[2]
-        mymd5 = hashlib.md5(open(path, 'rb').read()).hexdigest()
+        statinfo = os.stat(path)
+        filesize = statinfo.st_size
+        modtimestamp = statinfo.st_mtime
+        moddatetime = pd.to_datetime(modtimestamp, unit='s')
+        moddt_string = moddatetime.strftime("%Y%m%d_%H%M%S")
+        
 
         # query files for filename
-        filecheckrow = cur.execute("Select file_id, md5, record_id from source_files where srcfile=? and TIME_INVALID IS NULL",(filename,))
+        filecheckrow = cur.execute("Select file_id, md5 from manifest where filename=? and modtime=? and size=? and TIME_INVALID IS NULL",(filename,moddt_string,filesize))
         results = filecheckrow.fetchall()
         if len(results) > 1:
             print("Multiple active files with that filename - can't compare to old files")
@@ -117,39 +126,34 @@ def update_manifest(folderpath, databasename="journal.db"):
         elif len(results) == 1:
             results = results[0]
             oldfileid = results[0]
-            oldmd5 = results[1]
-            oldrecord = results[2]
+            # oldmd5 = results[1]
+            
 
             # if exists and md5 is different, invalidate existing file_id
             # then add new file like above, using new id, increment i_file
             # keep record the same
-            if oldmd5 != mymd5:
-                cur.execute("UPDATE source_files SET TIME_INVALID=? WHERE file_id=?", (dt_string, oldfileid,))
-                myargs = (i_file,oldrecord, personid,filename, mymd5,dt_string, None)
-                cur.executemany("INSERT INTO source_files VALUES(?, ?, ?, ?, ?, ?, ?)", [myargs])
-                i_file += 1
-                del activefilemap[oldfileid]
-            # if exists and md5 is same, remove from active-pending-delete list
-            else:
-                del activefilemap[oldfileid]
+            # if oldmd5 != mymd5:
+            #     cur.execute("UPDATE manifest SET TIME_INVALID=? WHERE file_id=?", (dt_string, oldfileid,))
+            #     myargs = (i_file, personid,filename,moddt_string,filesize, mymd5,dt_string, None,1)
+            #     cur.executemany("INSERT INTO manifest VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", [myargs])
+            #     i_file += 1
+            #     activefileset.remove(oldfileid)
+            # # if exists and md5 is same, remove from active-pending-delete list
+            # else:
+            #     activefileset.remove(oldfileid)
+            activefileset.remove(oldfileid)
         # if DNE, add new file
-        # add new record
         else:
-            myargs = (i_record, personid,i_file, mydate,mytime, "",1)
-            # print(myargs)
-            # print([type(item) for item in myargs])        
-            cur.executemany("INSERT INTO registry VALUES(?, ?, ?, ?, ?, ?, ?)", [myargs])
-            myargs = (i_file,i_record, personid,filename, mymd5,dt_string, None)
-            cur.executemany("INSERT INTO source_files VALUES(?, ?, ?, ?, ?, ?, ?)", [myargs])
+            mymd5 = hashlib.md5(open(path, 'rb').read()).hexdigest()
+            myargs = (i_file, personid,filename,moddt_string,filesize, mymd5,dt_string, None,1)
+            cur.executemany("INSERT INTO manifest VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", [myargs])
             i_file += 1
-            i_record += 1
 
     # for all files that were active but aren't there anymore
-    # invalidate in source_files, and invalidate in registry
+    # invalidate in manifest
     # for file_id,record_id in zip(activefilemap.keys(),activefilemap.values()):
-    cur.executemany("UPDATE registry SET ISACTIVE=0 WHERE record_id=?",tuple([(i,) for i in activefilemap.values()]))
-    myargs = map(lambda v: (dt_string,v), activefilemap.keys())
-    cur.executemany("UPDATE source_files SET TIME_INVALID=? WHERE file_id=?",myargs)
+    myargs = map(lambda v: (dt_string,v), activefileset)
+    cur.executemany("UPDATE manifest SET TIME_INVALID=? WHERE file_id=?",myargs)
 
 
     con.commit() 
